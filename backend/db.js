4c1620b452ba2e6b19ea12138b962fe79f3b4d84
IrinaSalikhova <irishkaakk@gmail.com>
2025-02-10 08:35:27 -0500

    gitignore

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,9 @@ const mysql = require('mysql2');
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+    //socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+    host: '34.29.63.110',
+    port: 3306,
     user: 'CATSforMAP',
     password: 'CATSforMAP1234!', 
     database: 'catmap'
