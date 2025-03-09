0a0d768073de24b26951c3e4516071160071b268
IrinaSalikhova <irishkaakk@gmail.com>
2025-03-09 17:19:51 -0400

    one back... was any more?

--- frontend/src/components/AddAssetForm.jsx
+++ frontend/src/components/AddAssetForm.jsx
@@ -1,76 +1,147 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import CategoryDropdown from "./CategoryDropdown";
 import "../assets/AddAssetForm.css";
-import requiredIcon from "/required.png";
-import AddAssetFormLocation from "./AddAssetFormLocation";
-import AddAssetFormContacts from "./AddAssetFormContacts";
 import AddAssetFormStep1 from "./AddAssetFormStep1";
+import AddAssetFormStep2 from "./AddAssetFormStep2";
+import AddAssetFormStep3 from "./AddAssetFormStep3";
+import AddAssetFormStep4 from "./AddAssetFormStep4";
 
+const STORAGE_KEY = "assetFormData";
 
-const NewAssetForm = ({ onClose, onSubmit, assetData, isLoggedIn, userRole, }) => {
+const NewAssetForm = ({ onClose, existingAssetData, userRole, isLoaded, loadError }) => {
   
-  const STORAGE_KEY = "assetFormData"; // TODO: Key for local storage
-  const [isNew, setIsNew] = useState(true);// TODO:
-
+  const [isNew, setIsNew] = useState(true); 
+  const [editingMode, setEditingMode] = useState(false);
 
   const [step, setStep] = useState(1);
   const [assetType, setAssetType] = useState({ multiple: false, physical: false });
-
-  const [showLocation, setShowLocation] = useState(false);
-  const [showContact, setShowContact] = useState(false);
-  const [showFormat, setShowFormat] = useState(false);
-
+  const [submissionStatus, setSubmissionStatus] = useState({ success: false, message: "" });
   const [formData, setFormData] = useState([
     {
-      categoryIds: [],
+      id: null,
       assetId: null,
+
       name: "",
+      categoryIds: [],
       description: "",
       isVolunOpp: false,
       volunOppText: "",
-      registrationNote: "",
-      scheduleNote: "",
-      isWheelchairAcc: false,
-      languagesOffered: [],
-      scheduleType: "",
       socialWorkerOnlyNote: "",
-      format: [],
-      createdEmail: "",
+
       cityName: "",
       address: "",
       postCode: "",
       transportation: "",
       longitude: null,
       latitude: null,
+
       email: [],
       phoneNumber: [],
-      website: []
+      website: [],
+
+      registrationNote: "",
+      scheduleNote: "",
+      isWheelchairAcc: false,
+      languagesOffered: [],
+      scheduleType: "",
+      format: [],
+
+      createdEmail: "",
+
     }
   ]);
 
+  useEffect(() => {
+
+    if (existingAssetData) { //TODO: need additional work when will be testible 
+      setEditingMode(true);
+      setFormData([existingAssetData]); 
+      setIsNew(false);
+    } else {
+      const savedData = localStorage.getItem(STORAGE_KEY);
+      if (savedData && isNew) {
+        const { step, assetType, formData } = JSON.parse(savedData);
+        setStep(step);
+        setAssetType(assetType);
+        setFormData(formData);
+        setIsNew(false);
+  }}}, [isNew, existingAssetData]);
+
+  useEffect(() => {
+    if (!isNew) {  
+      localStorage.setItem(STORAGE_KEY, JSON.stringify({ step, assetType, formData }));
+      console.log("Saving to localStorage:", formData); // Debugging log
+  }
+  }, [step, assetType, formData]);
+
+  const resetForm = () => {
+    setStep(1);
+    setAssetType({ multiple: false, physical: false });
+    setFormData([
+      {
+        assetId: null,
+        
+        name: "",
+        categoryIds: [],
+        description: "",
+        isVolunOpp: false,
+        volunOppText: "",
+        socialWorkerOnlyNote: "",        
+        
+        cityName: "",
+        address: "",
+        postCode: "",
+        transportation: "",
+        longitude: null,
+        latitude: null,
+        
+        email: [],
+        phoneNumber: [],
+        website: [],
+
+        registrationNote: "",
+        scheduleNote: "",
+        isWheelchairAcc: false,
+        languagesOffered: [],
+        scheduleType: "",
+        format: [],
+
+        createdEmail: "",
+      }
+    ]);
+    localStorage.removeItem(STORAGE_KEY);
+  };
+
+
+
+  const handleEmailEntered = (createdEmail) => {
+    setFormData(prevData =>
+      prevData.map(item =>
+        ({ ...item, createdEmail })
+        ));};
+
+
   const handleChange = (e, index, fieldIndex = null) => {
     const { value, type, checked, multiple, dataset } = e.target;
-    const field = dataset.field; // Correct field reference
+    const field = dataset.field;
   
     setFormData(prevData =>
-      prevData.map((item, i) =>
-        i === index
-          ? {
-              ...item,
-              [field]: fieldIndex !== null
-                ? item[field].map((val, idx) => (idx === fieldIndex ? value : val)) // Correct nested array handling
-                : type === "checkbox"
-                ? checked
-                : multiple
-                ? item[field].includes(value)
-                  ? item[field].filter(v => v !== value)
-                  : [...item[field], value]
-                : value
-            }
-          : item
-      )
-    );
-  };
+      prevData.map((item, i) => {
+        if (i === index) {
+          const updatedItem = { ...item };
+          if (fieldIndex !== null) {
+            updatedItem[field] = [...(updatedItem[field] || [])];
+            updatedItem[field][fieldIndex] = value;
+          } else {
+            updatedItem[field] = type === "checkbox" ? checked : value;
+          }
+          return updatedItem;
+        } 
+
+        return item;
+      }));};
+
+
 
 
   const handleCategorySelect = (categories, index, event) => {
@@ -80,209 +151,177 @@ const NewAssetForm = ({ onClose, onSubmit, assetData, isLoggedIn, userRole, }) =
         i === index
           ? { ...item, categoryIds: categories.map((cat) => cat.id) }
           : item
-      )
-    );
-  };
+      ));};
 
   const handleAssetTypeSelection = (type, value) => {
     setAssetType((prev) => ({ ...prev, [type]: value }));
-    if (type === "multiple" && !value) {
+    if (type === "multiple" && !value && isNew) {
       setFormData([formData[0]]);
     }
-    
+    setIsNew(false);
   };
  
   const isFormValid = (index) => {
     const service = formData[index];
-    return service.name.trim() !== "" && service.description.trim() !== "";
+    if (!service.name.trim() || service.categoryIds.length === 0) return false;
+
+    const isAddressEntered = (service.address && service.postCode && service.cityName) || 
+      (service.longitude !== null && service.latitude !== null);
+    const isContactInfoEntered = service.phoneNumber.length > 0 || service.email.length > 0 || service.website.length > 0;
+    return assetType.physical ? isAddressEntered : isContactInfoEntered;
   };
 
   const allFormsValid = formData.every((_, index) => isFormValid(index));
 
 
 
-  const handleAddService = () => {
+  const handleAddService = () => {  
     if (allFormsValid) {
       setFormData([
         ...formData,
-        {  ...formData[0],
-          categoryIds: [], 
+        {  ...formData[0], 
+          id: null,
           assetId: null,
+
           name: "",
           isVolunOpp: false,
           volunOppText: "",
-          registrationNote: "",
-          scheduleNote: "",
-          isWheelchairAcc: false,
-          languagesOffered: [],
-          scheduleType: "",
-          socialWorkerOnlyNote: "",
-          format: [],
+          description: `A service in ${formData[0].name}`,
           email: [],
           phoneNumber: [],
           website: [],
-          description: `A service in ${formData[0].name}`,
+
+          registrationNote: "",
+          isWheelchairAcc: false,
+          scheduleNote: "",
+          languagesOffered: [],
+          scheduleType: "",
+          format: [],
         }
       ]);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted Form Data:", formData);
-    if (onSubmit) {
-      onSubmit(formData);
+  const handleRemoveLastService = () => {
+    if (formData.length > 1) {
+      setFormData((prevData) => prevData.slice(0, -1));
     }
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (userRole === 'navigator') {
+      console.log("Navigator Submitted Form Data:", formData);
+      // here will be other routes later (for new and editing)
+
+
+
+
+
+    } else {
+      console.log("Form Data to submit:", formData);
+      if (editingMode) {
+        try {
+          // TODO: add editing of existing asset to draft (asset id should be populated)
+          console.log("edited asset Data to submit:", formData);
+        } catch (error) {
+          console.error("Error submitting asset:", error.message);
+          alert("Error submitting asset: " + error.message);
+        }
+      } else {
+      try {
+        const response = await fetch("/api/assets/suggestNewAsset", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ newAssetData: formData }),
+        });
+    
+        const result = await response.json();
+    
+        if (!response.ok) {
+          throw new Error(result.message || "Failed to submit asset data");
+        }
+    
+        console.log("Success:", result.message);
+        setSubmissionStatus({ success: true, message: result.message });
+        setStep(4);
+    
+      } catch (error) {
+        console.error("Error submitting asset:", error.message);
+        setSubmissionStatus({ success: false, message: error.message });
+        setStep(4);
+       } 
+      }
+    }
+    
+
+    return true; // add return !
+  };
+
+  // TODO: add back onhovers spans!
+
 
   return (
     <div className="modal-overlay">
       <div className="modal-container">
         <div className="header">
-          <h2>New Community Resource Information</h2>
+          <h2>
+            <span title="a resource, service, program or other activity in your community open to the public">
+              New Community Resource Information
+            </span>
+          </h2>
           <button className="close-button" onClick={onClose}>X</button>
         </div>
 
         <form id="suggestionForm" onSubmit={handleSubmit}>
           {step === 1 && (
-  <AddAssetFormStep1 
-    assetType={assetType} 
-    handleAssetTypeSelection={handleAssetTypeSelection} 
-    setStep={setStep} 
-  />
-)}
+            <AddAssetFormStep1 
+              assetType={assetType} 
+              handleAssetTypeSelection={handleAssetTypeSelection} 
+              setStep={setStep} 
+            />
+          )}
 
           {step === 2 && (
-            <div className="form-grid">
-              {formData.map((service, index) => (
-                <div key={index} className="service-section">
-                  <div className="asset-form-field-container">
-                    <label>
-                      Resource Name
-                      <img src={requiredIcon} alt="Required" className="required-icon" />
-                    </label>
-                    <textarea
-                      data-field="name"
-                      placeholder="ex. Carlington Community Health Centre"
-                      maxLength="250"
-                      required
-                      value={service.name}
-                      onChange={(e) => handleChange(e, index)}
-                    />
-                    <div className="character-count">
-                      {250 - service.name.length}/250
-                    </div>
-                  </div>
-
-                  <div className="asset-form-field-container">
-                  <CategoryDropdown onCategorySelect={(categories, event) => handleCategorySelect(categories, index, event)} />
-                  <img src={requiredIcon} alt="Required" className="required-icon" />
-                  
-                  </div>
-
-                  <div className="asset-form-field-container">
-                    <label>Description</label>
-                    <textarea
-                      data-field="description"
-                      placeholder="Enter a 50-200 word description"
-                      maxLength="1800"
-                      value={service.description}
-                      onChange={(e) => handleChange(e, index)}
-                    />
-                    <div className="character-count">
-                      {1800 - service.description.length}/1800
-                    </div>
-                  </div>
-
-                  <div className="asset-form-field-container">
-                    <label>Are there any volunteering opportunities?</label>
-                    <button
-                      type="button"
-                      className={`volunteer-button ${service.isVolunOpp ? "active" : ""}`}
-                      onClick={() =>
-                        setFormData((prevData) =>
-                          prevData.map((item, i) =>
-                            i === index ? { ...item, isVolunOpp: !item.isVolunOpp } : item
-                          )
-                        )
-                      }
-                    >
-                      Yes
-                    </button>
-
-                    {service.isVolunOpp && (
-                      <textarea
-                      data-field="volunOppText"
-                        placeholder="Please briefly list volunteering opportunities"
-                        maxLength="400"
-                        value={service.volunOppText}
-                        onChange={(e) => handleChange(e, index)}
-                      />
-                    )}
-                  </div>
-
-                  <div className="foldable-section">
-                    <button 
-                    type= "button"
-                    className="foldable-header" 
-                    onClick={() => setShowLocation(!showLocation)}>
-                    <div>
-                      <span>Location</span>
-                      {assetType.physical && <img src={requiredIcon} alt="Required" className="required-icon" />}
-                    </div>
-                <span className="foldable-arrow">{showLocation ? '▼' : '▶'}</span>
-              </button>
-
-              {showLocation && (
-  <AddAssetFormLocation 
-    handleChange={handleChange} 
-    service={service} 
-    index={index} 
-  />
-)}
-            </div>
-
-
-            <div className="foldable-section">
-                    <button 
-                    type= "button"
-                    className="foldable-header" 
-                    onClick={() => setShowContact(!showContact)}>
-                    <div>
-                      <span>Contact Information</span>
-                      {!assetType.physical && <img src={requiredIcon} alt="Required" className="required-icon" />}
-                    </div>
-                <span className="foldable-arrow">{showContact ? '▼' : '▶'}</span>
-              </button>
-            {showContact && (
-              <AddAssetFormContacts 
-              handleChange={handleChange} 
-              service={service} 
-              index={index} 
+            <AddAssetFormStep2
+              formData={formData}
+              handleChange={handleChange}
+              handleCategorySelect={handleCategorySelect}
+              assetType={assetType}
+              handleAddService={handleAddService}
+              allFormsValid={allFormsValid}
               setFormData={setFormData}
+              resetForm={resetForm}
+              setStep={setStep}
+              handleRemoveLastService={handleRemoveLastService}
+              isNavigator={userRole === 'navigator'}
+              handleSubmit={handleSubmit}
+              isLoaded={isLoaded}
+              loadError={loadError}
             />
-)}
-             
-            </div>
-
-
-                </div>
-              ))}
-
-              {assetType.multiple && (
-                <button
-                  type="button"
-                  onClick={handleAddService}
-                  disabled={!allFormsValid}
-                  className={!allFormsValid ? "disabled-button" : ""}
-                >
-                  + Add Another Service or Program
-                </button>
-              )}
-
-              <button type="submit">Submit</button>
-            </div>
           )}
+
+          {step === 3 && (
+            <AddAssetFormStep3
+              handleEmailEntered={handleEmailEntered}
+              handleSubmit={handleSubmit}
+              setStep={setStep}
+            />
+          )}
+
+          {step === 4 && (
+            <AddAssetFormStep4
+              onClose={onClose}
+              resetForm={resetForm}
+              isNavigator={userRole === 'navigator'}
+              submissionStatus={submissionStatus}
+              setStep={setStep}
+            />
+          )}
+
         </form>
       </div>
     </div>
